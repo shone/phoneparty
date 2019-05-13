69554dbe2925dc66fa5a9a5a99e5f653b924680d
Josh Shone <josh.shone@gmail.com>
2019-05-13 09:41:07 +0200

    Random player selection and loooots of other stuff

--- server.go
+++ server.go
@@ -5,11 +5,17 @@ import (
   "github.com/gorilla/websocket"
   "sync"
   "sync/atomic"
-  "strconv"
   "encoding/json"
 )
 
+type ClientMessage struct {
+  ClientId uint64 `json:"clientId"`
+  Type     string `json:"type"`
+  Message  string `json:"message"`
+}
+
 func main() {
+  // Serve files
   http.Handle("/", http.FileServer(http.Dir(".")))
 
   var hostConn *websocket.Conn
@@ -55,49 +61,65 @@ func main() {
         log.Println("Unexpected message type received from host websocket")
       } else {
         log.Println("Received message from host, relaying to client..")
-        clientIdAndMessage := []string{"",""}
-        json.Unmarshal(message, &clientIdAndMessage)
-        clientId,_ := strconv.ParseUint(clientIdAndMessage[0], 10, 64)
-        clientConnI,_ := clientConns.Load(clientId)
-        clientConn := clientConnI.(*websocket.Conn)
-        err = clientConn.WriteMessage(websocket.TextMessage, []byte(clientIdAndMessage[1]))
-        if err != nil {
+        clientMessage := ClientMessage{}
+        json.Unmarshal(message, &clientMessage)
+        clientConn,found := clientConns.Load(clientMessage.ClientId)
+        if !found {
+          log.Println("Received message from host for client ", clientMessage.ClientId, " but the client could not be found")
+          continue
+        }
+        err = (clientConn.(*websocket.Conn)).WriteMessage(websocket.TextMessage, []byte(clientMessage.Message))
+        if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
+          log.Println("Client", clientMessage.ClientId, "websocket closed unexpectedly")
+          clientConns.Delete(clientMessage.ClientId)
+          messageToHost,_ := json.Marshal(ClientMessage{ClientId: clientMessage.ClientId, Type: "clientDisconnected"})
+          conn.WriteMessage(websocket.TextMessage, messageToHost)
+          continue
+        } else if websocket.IsCloseError(err, websocket.CloseGoingAway) {
+          log.Println("Client", clientMessage.ClientId, "websocket closed")
+          clientConns.Delete(clientMessage.ClientId)
+          messageToHost,_ := json.Marshal(ClientMessage{ClientId: clientMessage.ClientId, Type: "clientDisconnected"})
+          conn.WriteMessage(websocket.TextMessage, messageToHost)
+          continue
+        } else if err != nil {
           log.Println("Error while attempting to write to client websocket: ", err)
         }
       }
     }
   })
   http.HandleFunc("/client", func(response http.ResponseWriter, request *http.Request) {
+    if hostConn == nil {
+      log.Println("A client attempted to connect without a host being connected")
+      response.WriteHeader(http.StatusNotFound)
+      response.Write([]byte("There is currently no host to connect to"))
+      return
+    }
     conn,err := upgrader.Upgrade(response, request, nil)
     if err != nil {
       log.Println("Unable to upgrade client connection to websocket: ", err)
       return
     }
-    log.Println("Client connected")
     clientId := atomic.AddUint64(&nextClientId, 1)
+    log.Println("Client", clientId, "connected")
     clientConns.Store(clientId, conn)
 
-    // Send client ID
-//     conn.WriteMessage(websocket.TextMessage, strconv.FormatInt(clientId, 10))
-    
     messageType,clientSdp,err := conn.ReadMessage()
     if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-      log.Println("Client websocket closed unexpectedly")
+      log.Println("Client", clientId, "websocket closed unexpectedly")
       clientConns.Delete(clientId)
       return
     } else if websocket.IsCloseError(err, websocket.CloseGoingAway) {
-      log.Println("Client websocket closed")
+      log.Println("Client", clientId, "websocket closed")
       clientConns.Delete(clientId)
       return
-    }
-    if err != nil {
+    } else if err != nil {
       log.Println("Error while attempting to read SDP from client: ", err)
     }
     if messageType != websocket.TextMessage {
       log.Println("Unexpected message type received from client websocket")
     }
     log.Println("Client SDP received, relaying to host..")
-    messageWithClientId,_ := json.Marshal([]string{strconv.FormatUint(clientId, 10), string(clientSdp)})
+    messageWithClientId,_ := json.Marshal(ClientMessage{ClientId: clientId, Message: string(clientSdp)})
     err = hostConn.WriteMessage(websocket.TextMessage, messageWithClientId)
     if err != nil {
       log.Println("Unable to relay client SDP to host: ", err)
@@ -105,23 +127,33 @@ func main() {
     for {
       messageType,message,err := conn.ReadMessage()
       if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway) {
-        log.Println("Client websocket closed unexpectedly")
+        log.Println("Client", clientId, "websocket closed unexpectedly")
         clientConns.Delete(clientId)
+        if hostConn != nil {
+          messageToHost,_ := json.Marshal(ClientMessage{ClientId: clientId, Type: "clientDisconnected"})
+          hostConn.WriteMessage(websocket.TextMessage, messageToHost)
+        }
         return
       } else if websocket.IsCloseError(err, websocket.CloseGoingAway) {
-        log.Println("Client websocket closed")
+        log.Println("Client", clientId, "websocket closed")
         clientConns.Delete(clientId)
+        if hostConn != nil {
+          messageToHost,_ := json.Marshal(ClientMessage{ClientId: clientId, Type: "clientDisconnected"})
+          hostConn.WriteMessage(websocket.TextMessage, messageToHost)
+        }
         return
-      }
-      if err != nil {
+      } else if err != nil {
         log.Println("Error while reading from client websocket: ", err)
         continue
       }
       if messageType != websocket.TextMessage {
         log.Println("Received unexpected message type from host websocket")
       }
-      messageWithClientId,_ = json.Marshal([]string{strconv.FormatUint(clientId, 10), string(message)})
+      messageWithClientId,_ = json.Marshal(ClientMessage{ClientId: clientId, Message: string(message)})
       log.Println("Received message from client, relaying to host..")
+      if hostConn == nil {
+        return
+      }
       err = hostConn.WriteMessage(websocket.TextMessage, messageWithClientId)
       if err != nil {
         log.Println("Unable to write to host websocket: ", err)
