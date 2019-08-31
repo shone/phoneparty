7bb197c8afb76970fa5a4eaf73b46deba8ab362b
Jannis Baudisch <github@jannisbaudisch.de>
2019-08-31 18:49:56 +0200

    Phase three voting is kinda done clientside

--- host/phase3.js
+++ host/phase3.js
@@ -1,45 +1,50 @@
-NUMBER_OF_IMAGES = 5
-
 async function phase3() {
-  console.log("Start Phase Three");
+  var phase = new function() {
+    this.NUMBER_OF_IMAGES = 5
 
-  await showText('Waiting for Phase 3...', 3, 'gray');
+    this.sendImagesToPlayers = async function() {
+      //clientid, image
+      var images = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0};
+      var player_list = [...players];
 
-  await sendImagesToPlayers();
+      //Shuffle player list
+      player_list.sort(() => Math.random() - 0.5);
 
+      for(player_index in player_list) {
+        console.log("Send data to player");
+        var player = player_list[player_index];
+        console.log(this);
+        //Add onmessage callback to every player
+        player.phaseThree.onmessage = this.onPlayerVoteResponse.bind(this);
 
-  await showText('Phase 3', 999, 'blue');
-}
+        // For image
+        var data = [];
 
+        for(var i = 0; i < this.NUMBER_OF_IMAGES; i++) {
+          var image_owner = (player_index + 1 + i); // % player_list.length
 
-async function sendImagesToPlayers() {
-  //clientid, image
-  var images = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0};
-  var player_list = [...players];
+          data.push({"image_owner": image_owner, "image_blob": images[image_owner]});
+        }
 
-  //Shuffle player list
-  player_list.sort(() => Math.random() - 0.5);
+        var string_data = JSON.stringify(data)
+        console.log(string_data);
 
-  for(player_index in player_list) {
-    console.log("Send data to player");
-    var player = player_list[player_index];
-
-    // For image
-    var data = [];
-
-    for(var i = 0; i < this.NUMBER_OF_IMAGES; i++) {
-      var image_owner = (player_index + 1 + i); // % player_list.length
-
-      data.push({"image_owner": image_owner, "image_blob": images[image_owner]});
+        player.phaseThree.send(string_data)
+      }
     }
 
-    var string_data = JSON.stringify(data)
-    console.log(string_data);
+    this.onPlayerVoteResponse = function(event) {
+      console.log(event.data);
+    }
 
-    player.phaseThree.send(string_data)
+    this.start = async function() {
+      console.log("Start Phase Three");
+
+      await this.sendImagesToPlayers();
+
+      await showText('Phase 3', 999, 'blue');
+    }
   }
+
+  await phase.start();
 }
-
-
-// Broadcast new phase
-// Send images to players
--- player/phase3.js
+++ player/phase3.js
@@ -1,106 +1,120 @@
 function phase3(channels) {
-  console.log("Starting phase 3");
+  new function(channels) {
+    console.log("Starting phase 3");
 
-  this.imageTypes = {REAL: 0, FAKE: 1, WRONG_COLOR: 2};
+    this.imageTypes = {REAL: 0, FAKE: 1, WRONG_COLOR: 2};
 
-  // Add callbacks
-  this.onPhaseThreeMessage = function(event) {
-    this.data = JSON.parse(event.data);
-    this.image_counter = 0;
-    console.log(this.data);
+    // Add callbacks
+    this.onPhaseThreeMessage = function(event) {
+      this.data = JSON.parse(event.data);
+      this.image_counter = 0;
+      console.log(this.data);
 
-    this.response = [];
+      this.response = [];
 
-    console.log(this);
-    this.displayNextImage();
-  }.bind(this);
+      this.displayNextImage();
+    }.bind(this);
 
 
-  this.onKeyPress = function(event) {
-    switch (event.code) {
-      case "KeyA":
-        this.onImageIsReal();
-        break;
-      case "KeyD":
-        this.onImageIsFake();
-        break;
-      case "KeyS":
-        this.onImageIsWrongColor();
-        break;
-    }
-  }
-
-  this.createDisplay = function() {
-    var container = document.createElement("div");
-    document.body.append(container);
-
-    this.preview = document.createElement("div");
-    container.append(this.preview);
-
-    this.preview.image = document.createElement("img");
-
-    this.preview.append(this.preview.image);
-  }
-
-  this.displayNextImage = function() {
-    this.block_input = false;
-    console.log("display next image", this.image_counter);
-
-    // Remove swipe classes
-    this.preview.image.classList.remove("swipeLeft")
-    this.preview.image.classList.remove("swipeRight")
-    this.preview.image.classList.remove("swipeUp")
-
-    var next_image = this.getNextImage();
-
-    if(next_image == null) {
-      console.log("No images left, send data to server");
-      var string_data = JSON.stringify(this.response);
-      channels.phaseThree.send(string_data);
+    this.onKeyPress = function(event) {
+      switch (event.code) {
+        case "KeyA":
+          this.onImageIsReal();
+          break;
+        case "KeyD":
+          this.onImageIsFake();
+          break;
+        case "KeyW":
+          this.onImageIsWrongColor();
+          break;
+      }
     }
 
-    this.preview.image.src = "https://loremflickr.com/cache/resized/65535_48234486291_3d66356db3_350_225_nofilter.jpg";
-  }
+    this.createDisplay = function() {
+      //Remove existing things
+      var phase1 = document.getElementById("phase1");
+      if(phase1) {phase1.remove();}
 
-  this.getNextImage = function() {
-    if (this.image_counter < this.data.length) {
-      return this.data[this.image_counter];
+      var container = document.createElement("div");
+      document.body.append(container);
+      container.id = "phase3"
+
+      this.preview = document.createElement("div");
+      container.append(this.preview);
+      this.preview.style.overflow = "hidden";
+      this.preview.style.height = "60%";
+      this.preview.style.width = "60%";
+
+      this.preview.image = document.createElement("img");
+
+      this.preview.append(this.preview.image);
     }
-    return null;
-  }
 
-  this.onImageIsReal = function() {
-    if(this.block_input) {return;}
+    this.displayNextImage = function() {
+      this.block_input = false;
+      console.log("display next image");
 
-    console.log("real");
-    this.preview.image.classList.add("swipeLeft");
-    this.setCurrentImageAs(this.imageTypes.REAL);
-  }
+      // Remove swipe classes
+      this.preview.image.classList.remove("swipeLeft")
+      this.preview.image.classList.remove("swipeRight")
+      this.preview.image.classList.remove("swipeUp")
 
-  this.onImageIsFake = function() {
-    if(this.block_input) {return;}
+      var next_image = this.getNextImage();
 
-    console.log("Fake");
-    this.preview.image.classList.add("swipeRight");
-    this.setCurrentImageAs(this.imageTypes.FAKE);
-  }
+      if(next_image == null) {
+        if(!this.response_send) {
+          console.log("No images left, send data to server");
+          var string_data = JSON.stringify(this.response);
+          channels.phaseThree.send(string_data);
 
-  this.onImageIsWrongColor = function() {
-    if(this.block_input) {return;}
+          this.response_send = true;
+        }
+      } else {
+        this.preview.image.src = "https://loremflickr.com/cache/resized/65535_48234486291_3d66356db3_350_225_nofilter.jpg";
+      }
+    }
 
-    console.log("Wrong color");
-    this.preview.image.classList.add("swipeUp");
-    this.setCurrentImageAs(this.imageTypes.WRONG_COLOR);
-  }
+    this.getNextImage = function() {
+      if (this.image_counter < this.data.length) {
+        console.log("Image " + this.image_counter + " out of " + this.data.length);
+        return this.data[this.image_counter++];
+      }
+      return null;
+    }
 
-  this.setCurrentImageAs = function(type) {
-    this.response.push({"owner_id": this.data[this.image_counter].owner_id, "result": type});
+    this.onImageIsReal = function() {
+      if(this.block_input) {return;}
 
-    window.setTimeout(this.displayNextImage.bind(this), 1000);
-    this.block_input = true;
-  }
+      console.log("real");
+      this.preview.image.classList.add("swipeLeft");
+      this.setCurrentImageAs(this.imageTypes.REAL);
+    }
 
-  this.createDisplay();
-  document.addEventListener("keypress", this.onKeyPress.bind(this));
-  channels.phaseThree.onmessage = this.onPhaseThreeMessage;
+    this.onImageIsFake = function() {
+      if(this.block_input) {return;}
+
+      console.log("Fake");
+      this.preview.image.classList.add("swipeRight");
+      this.setCurrentImageAs(this.imageTypes.FAKE);
+    }
+
+    this.onImageIsWrongColor = function() {
+      if(this.block_input) {return;}
+
+      console.log("Wrong color");
+      this.preview.image.classList.add("swipeUp");
+      this.setCurrentImageAs(this.imageTypes.WRONG_COLOR);
+    }
+
+    this.setCurrentImageAs = function(type) {
+      this.response.push({"owner_id": this.data[this.image_counter - 1].image_owner, "result": type});
+
+      window.setTimeout(this.displayNextImage.bind(this), 1000);
+      this.block_input = true;
+    }
+
+    this.createDisplay();
+    document.addEventListener("keypress", this.onKeyPress.bind(this));
+    channels.phaseThree.onmessage = this.onPhaseThreeMessage;
+  }(channels);
 }
