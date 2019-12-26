e3941e3ca89bf2943e8704aec90b8d317cc47837
Josh Shone <josh.shone@gmail.com>
2019-12-26 16:46:35 +0100

    Small improvement to all-the-things title screen
    Separately animate forground/background parts of title screen image

--- games/all-the-things/host/titleScreen.mjs
+++ games/all-the-things/host/titleScreen.mjs
@@ -3,6 +3,7 @@ import {waitForNSeconds, waitForKeypress} from '/shared/utils.mjs';
 export default async function titleScreen() {
   document.body.insertAdjacentHTML('beforeend', `
     <div class="all-the-things title-screen">
+      <div class="title-image-background"></div>
       <div class="title-image"></div>
       <h1>All The Things</h1>
       <h2>the object finding game</h2>
