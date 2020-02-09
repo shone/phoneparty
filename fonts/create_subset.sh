a1fdc439584ed5ef83573c427db932af029b266b
Josh Shone <josh.shone@gmail.com>
2020-02-09 15:34:27 +0100

    Update emoji font subset and make shell script

---
+++ fonts/create_subset.sh
@@ -0,0 +1,4 @@
+# Create font with only emoji characters needed by Phone Party
+pyftsubset NotoColorEmoji.woff2 --text="👍👎👌😄😆😅🤣😂🙂😉😇☺️😛🥰🤔🤫🤨😬😏😌😴😟🙁😯😥👋✌️🤞" --flavor="woff2"
+
+# See also: https://emojipedia.org/whatsapp/
