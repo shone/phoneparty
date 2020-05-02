50ca24d451600d356c6222241b6f7cec19bc96a3
Josh Shone <josh.shone@gmail.com>
2020-05-02 12:47:06 +0200

    Fixes for photo taking screen
    - Ensure taken photo remains on-screen until route changes
    - Fix switching of front/rear camera
    - Use push-button for camera button
    - Comments/cleanups

--- player/main.mjs
+++ player/main.mjs
@@ -39,6 +39,7 @@ navigator.serviceWorker.register('/service-worker.js');
 location.hash = '';
 
 export let stream = null;
+export let rtcConnection = null;
 
 (async function main() {
 
@@ -177,7 +178,7 @@ function setupFullscreenButton() {
 async function connectRtcAndStartGame(websocket) {
   showStatus('waiting', 'Establishing WebRTC connection', 'Setting up connection object');
 
-  var rtcConnection = new RTCPeerConnection();
+  rtcConnection = new RTCPeerConnection();
 
   if (stream) {
     stream.getTracks().forEach(track => rtcConnection.addTrack(track, stream));
