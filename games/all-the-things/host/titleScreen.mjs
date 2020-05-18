f10ea9067c5ca7e2756380c0268a920dcfb8b7cb
Josh Shone <josh.shone@gmail.com>
2020-05-18 15:19:12 +0200

    Clean-up Tunnel Vision intro code and improve 'closeup trickery' reveal animation

--- games/all-the-things/host/titleScreen.mjs
+++ games/all-the-things/host/titleScreen.mjs
@@ -30,10 +30,27 @@ routes['#games/all-the-things'] = async function titleScreen() {
   audienceMode.start();
   messaging.start();
 
+  const tunnelEffect = createTunnelEffect(titleScreen.querySelector('canvas'));
+
+  await waitForRouteToEnd();
+
+  tunnelEffect.stop();
+
+  titleScreen.classList.add('finished');
+  setTimeout(() => { titleScreen.remove() }, 2000);
+  await Promise.race([waitForNSeconds(2), waitForKeypress(' ')]);
+
+  messaging.stop();
+
+  if (!location.hash.startsWith('#games/all-the-things')) {
+    audienceMode.stop();
+  }
+  return '#games/all-the-things/thing-choosing';
+}
+
+function createTunnelEffect(canvas) {
   const tunnelJourneyDurationMs = 5000;
   const fieldOfViewYrad = 45 * Math.PI / 180;
-
-  const canvas = titleScreen.querySelector('canvas');
 
   const gl = canvas.getContext('webgl');
   if (!gl) {
@@ -43,100 +60,92 @@ routes['#games/all-the-things'] = async function titleScreen() {
 
   gl.getExtension('OES_standard_derivatives');
 
-  const fullscreenQuadVertexShader = loadShader(gl, gl.VERTEX_SHADER, `
-    attribute vec4 vertex_position;
-    void main() { gl_Position = vertex_position; }
-  `);
+  const lensFlareShaderProgram = createGlProgram(gl, {
+    vertexShaderSource: `
+      attribute vec4 vertex_position;
+      void main() { gl_Position = vertex_position; }
+    `,
+    fragmentShaderSource: `
+      precision mediump float;
 
-  const lensFlareFragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, `
-    precision lowp float;
-    uniform vec2 resolution;
-    uniform float timeMs;
+      uniform vec2 resolution;
+      uniform float timeMs;
 
-    void main() {
-      vec2 uv = (gl_FragCoord.xy / resolution) - vec2(.5,.5);
-      float PI = 3.14159;
-      float angle = 1.0 + (atan(uv.x, uv.y) / PI);
-      float warble1 = sin(angle * 16.0);
-      float warble2 = cos(angle * 20.0);
-      float warble3 = sin((angle + (timeMs / 3000.0)) * 35.0);
-      float warbles = (warble1 + (warble2*.5) + (warble3*.2)) / 12.0;
-      float dist = 1.0 - length(uv);
-      float fadein = min(timeMs / 1000.0, 1.0);
-      float fadeout = 1.0 - (timeMs / 5000.0);
-      float intensity = (pow(dist, 10.0) + warbles) * (fadeout * fadein);
-      gl_FragColor = vec4(intensity,intensity,intensity,intensity);
-    }
-  `);
+      void main() {
+        vec2 uv = (gl_FragCoord.xy / resolution) - vec2(.5,.5);
+        const float PI = 3.14159;
+        float angle = 1.0 + (atan(uv.x, uv.y) / PI);
+        float warble1 = sin(angle * 16.0);
+        float warble2 = cos(angle * 20.0);
+        float warble3 = sin((angle + (timeMs / 3000.0)) * 35.0);
+        float warbles = (warble1 + (warble2*.5) + (warble3*.2)) / 12.0;
+        float dist = 1.0 - length(uv);
+        float fadein = min(timeMs / 1000.0, 1.0);
+        float fadeout = 1.0 - (timeMs / 5000.0);
+        float intensity = (pow(dist, 10.0) + warbles) * (fadeout * fadein);
+        gl_FragColor = vec4(intensity);
+      }
+    `
+  });
 
-  const lensFlareShaderProgram = gl.createProgram();
-  gl.attachShader(lensFlareShaderProgram, fullscreenQuadVertexShader);
-  gl.attachShader(lensFlareShaderProgram, lensFlareFragmentShader);
-  gl.linkProgram(lensFlareShaderProgram);
-  gl.useProgram(lensFlareShaderProgram);
   const aFullscreenQuadVertexPosition = gl.getAttribLocation(lensFlareShaderProgram, 'vertex_position');
   const uResolution = gl.getUniformLocation(lensFlareShaderProgram, 'resolution');
+  gl.useProgram(lensFlareShaderProgram);
   gl.uniform2f(uResolution, canvas.clientWidth, canvas.clientHeight);
   const uTimeMs = gl.getUniformLocation(lensFlareShaderProgram, 'timeMs');
 
   const fullscreenQuadVertexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, fullscreenQuadVertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    -1, -1, 0,
-     1, -1, 0,
-    -1,  1, 0,
-     1,  1, 0,
+  gl.bufferData(gl.ARRAY_BUFFER, new Int8Array([
+    -1, -1, // Top-left corner
+     1, -1, // Top-right corner
+    -1,  1, // Bottom-left corner
+     1,  1, // Bottom right corner
   ]), gl.STATIC_DRAW);
 
-  const tunnelVertexShader = loadShader(gl, gl.VERTEX_SHADER, `
-    attribute vec4 aVertexPosition;
+  const tunnelShaderProgram = createGlProgram(gl, {
+    vertexShaderSource: `
+      attribute vec4 aVertexPosition;
 
-    uniform mat4 uModelViewMatrix;
-    uniform mat4 uProjectionMatrix;
+      uniform float uCameraZ;
+      uniform mat4 uProjectionMatrix;
 
-    varying vec3 vertex_view_space;
+      varying vec3 vertex_view_space;
 
-    void main() {
-      vertex_view_space = aVertexPosition.xyz;
-      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    }
-  `);
-  const tunnelFragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, `
-    #extension GL_OES_standard_derivatives : enable
+      void main() {
+        vertex_view_space = aVertexPosition.xyz;
+        vec4 vertexPosition = aVertexPosition;
+        vertexPosition.z += uCameraZ;
+        gl_Position = uProjectionMatrix * vertexPosition;
+      }
+    `,
+    fragmentShaderSource: `
+      #extension GL_OES_standard_derivatives : enable
+      precision mediump float;
 
-    varying lowp vec3 vertex_view_space;
+      varying vec3 vertex_view_space;
 
-    const lowp float tunnelDepth = 20.0;
-    const lowp vec3 diffuse = vec3(.3, .2, .03);
-    const lowp vec3 lightDirection = vec3(0, 0, -1);
+      const float tunnelDepth = 20.0;
+      const vec3 diffuse = vec3(.3, .2, .03);
+      const vec3 lightDirection = vec3(0, 0, -1);
 
-    void main() {
-      lowp vec3 normal = normalize(cross(dFdx(vertex_view_space), dFdy(vertex_view_space)));
-      lowp float incidence = max(dot(normal, lightDirection), 0.0);
-      lowp float tunnelEndCloseness = 1.0 - (vertex_view_space.z / tunnelDepth);
-      gl_FragColor.a = 1.0;
-      gl_FragColor.rgb = diffuse * tunnelEndCloseness * (0.5 + incidence);
-    }
-  `);
-
-  const tunnelShaderProgram = gl.createProgram();
-  gl.attachShader(tunnelShaderProgram, tunnelVertexShader);
-  gl.attachShader(tunnelShaderProgram, tunnelFragmentShader);
-  gl.linkProgram(tunnelShaderProgram);
-  gl.useProgram(tunnelShaderProgram);
-
-  if (!gl.getProgramParameter(tunnelShaderProgram, gl.LINK_STATUS)) {
-    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(tunnelShaderProgram));
-    return;
-  }
+      void main() {
+        vec3 normal = normalize(cross(dFdx(vertex_view_space), dFdy(vertex_view_space)));
+        float incidence = max(dot(normal, lightDirection), 0.0);
+        float tunnelEndCloseness = 1.0 - (vertex_view_space.z / tunnelDepth);
+        gl_FragColor.a = 1.0;
+        gl_FragColor.rgb = diffuse * tunnelEndCloseness * (0.5 + incidence);
+      }
+    `
+  });
 
   const aVertexPosition   = gl.getAttribLocation(tunnelShaderProgram, 'aVertexPosition'),
         uProjectionMatrix = gl.getUniformLocation(tunnelShaderProgram, 'uProjectionMatrix'),
-        uModelViewMatrix  = gl.getUniformLocation(tunnelShaderProgram, 'uModelViewMatrix');
+        uCameraZ          = gl.getUniformLocation(tunnelShaderProgram, 'uCameraZ');
 
   const tunnelDepth = 20;
 
-  const {positions, indices} = createTunnel({depth: tunnelDepth, depthSubdivs: 200, radialSubdivs: 40});
+  const {positions, indices} = createTunnelMesh({depth: tunnelDepth, depthSubdivs: 200, radialSubdivs: 40});
 
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -160,26 +169,25 @@ routes['#games/all-the-things'] = async function titleScreen() {
   }
   window.addEventListener('resize', onWindowResize);
 
-  const modelViewMatrix = createModelViewMatrix(0, 0, -tunnelDepth);
-
   gl.blendFunc(gl.ONE, gl.ONE);
 
-  let routeEnded = false;
+  let stop = false;
 
   const startTimestamp = performance.now();
   let previousTimestamp = startTimestamp;
   requestAnimationFrame(function callback(timestamp) {
     const journeyCompletionRatio = (timestamp - startTimestamp) / tunnelJourneyDurationMs;
 
-    gl.clearColor(0, 0, 0, Math.max(1 - (journeyCompletionRatio / 0.1), 0));
+    const fadeIn = Math.max(1 - (journeyCompletionRatio / 0.1), 0);
+    gl.clearColor(0, 0, 0, fadeIn);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.useProgram(tunnelShaderProgram);
     gl.disable(gl.BLEND);
 
     const translation = -tunnelDepth * (1 - journeyCompletionRatio);
-    setModelViewMatrixTranslation(modelViewMatrix, [0, 0, translation]);
-    gl.uniformMatrix4fv(uModelViewMatrix, false, modelViewMatrix);
+    gl.uniform1f(uCameraZ, translation);
+    console.log(translation);
 
     perspectiveSkew = 0.01 + journeyCompletionRatio;
     setProjectionMatrixSkew(projectionMatrix, fieldOfViewYrad, aspect, perspectiveSkew);
@@ -196,55 +204,38 @@ routes['#games/all-the-things'] = async function titleScreen() {
     gl.enable(gl.BLEND);
     gl.bindBuffer(gl.ARRAY_BUFFER, fullscreenQuadVertexBuffer);
     gl.enableVertexAttribArray(aFullscreenQuadVertexPosition);
-    gl.vertexAttribPointer(aFullscreenQuadVertexPosition, 3 /*components*/, gl.FLOAT, false, 0 /*normalize*/, 0 /*offset*/);
+    gl.vertexAttribPointer(aFullscreenQuadVertexPosition, 2 /*components*/, gl.BYTE, false, 0 /*normalize*/, 0 /*offset*/);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
-    if (journeyCompletionRatio < 1 && !routeEnded) {
+    if (journeyCompletionRatio < 1 && !stop) {
       requestAnimationFrame(callback);
     }
   });
 
-  let skew = 1;
-  let translation = -20;
-  window.addEventListener('mousedown', event => {
-     let lastY = event.pageY;
-     const button = event.button;
-     function handleMousemove(event) {
-      const deltaY = event.pageY - lastY;
-      lastY = event.pageY;
-      if (button === 0) {
-        translation += deltaY * 0.2;
-        setModelViewMatrixTranslation(modelViewMatrix, [0, 0, translation]);
-        gl.uniformMatrix4fv(uModelViewMatrix,  false, modelViewMatrix);
-      } else if (button === 1) {
-        skew += deltaY * 0.1;
-        console.log(skew);
-        const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-        setProjectionMatrixSkew(projectionMatrix, fieldOfViewYrad, aspect, skew);
-      }
-     }
-     window.addEventListener('mousemove', handleMousemove);
-     window.addEventListener('mouseup', () => {
-       window.removeEventListener('mousemove', handleMousemove);
-     }, {once: true});
-  });
-
-  await waitForRouteToEnd();
-  routeEnded = true;
-
-  window.removeEventListener('resize', onWindowResize);
-
-  titleScreen.classList.add('finished');
-  setTimeout(() => { titleScreen.remove() }, 2000);
-  await Promise.race([waitForNSeconds(2), waitForKeypress(' ')]);
-
-  messaging.stop();
-
-  if (!location.hash.startsWith('#games/all-the-things')) {
-    audienceMode.stop();
+  return {
+    stop() {
+      stop = true;
+      window.removeEventListener('resize', onWindowResize);
+    }
   }
-  return '#games/all-the-things/thing-choosing';
+}
+
+function createGlProgram(gl, {vertexShaderSource, fragmentShaderSource}) {
+  const program = gl.createProgram();
+
+  const vertexShader   = loadShader(gl, gl.VERTEX_SHADER,   vertexShaderSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(program));
+    return null;
+  }
+
+  return program;
 }
 
 function loadShader(gl, type, source) {
@@ -280,66 +271,7 @@ function setProjectionMatrixSkew(mat4, fovy, aspect, skew) {
   mat4[5] = f;
 }
 
-function createModelViewMatrix(x, y, z) {
-  const mat4 = new Float32Array(16);
-  mat4[0]  = 1;
-  mat4[5]  = 1;
-  mat4[10] = 1;
-  mat4[15] = 1;
-  mat4[12] = x * y * z;
-  mat4[13] = y;
-  mat4[14] = z;
-  return mat4;
-}
-
-function setModelViewMatrixTranslation(mat4, [x, y, z]) {
-  mat4[12] = x * y * z;
-  mat4[13] = y;
-  mat4[14] = z;
-}
-
-// function translateMatrix(mat4, [x, y, z]) {
-//   mat4[12] = mat4[0] * x + mat4[4] * y + mat4[8]  * z + mat4[12];
-//   mat4[13] = mat4[1] * x + mat4[5] * y + mat4[9]  * z + mat4[13];
-//   mat4[14] = mat4[2] * x + mat4[6] * y + mat4[10] * z + mat4[14];
-//   mat4[15] = mat4[3] * x + mat4[7] * y + mat4[11] * z + mat4[15];
-// }
-
-// function rotateMatrix(mat4, rad, [x, y, z]) {
-//   const len = 1 / Math.sqrt(x * x + y * y + z * z);
-//   x *= len;
-//   y *= len;
-//   z *= len;
-// 
-//   const s = Math.sin(rad),
-//         c = Math.cos(rad),
-//         t = 1 - c;
-// 
-//   const a00 = mat4[0], a01 = mat4[1], a02 = mat4[2],  a03 = mat4[3],
-//         a10 = mat4[4], a11 = mat4[5], a12 = mat4[6],  a13 = mat4[7],
-//         a20 = mat4[8], a21 = mat4[9], a22 = mat4[10], a23 = mat4[11];
-// 
-//   // Construct the elements of the rotation matrix
-//   const b00 = x * x * t + c,     b01 = y * x * t + z * s, b02 = z * x * t - y * s,
-//         b10 = x * y * t - z * s, b11 = y * y * t + c,     b12 = z * y * t + x * s,
-//         b20 = x * z * t + y * s, b21 = y * z * t - x * s, b22 = z * z * t + c;
-// 
-//   // Perform rotation-specific matrix multiplication
-//   mat4[0]  = a00 * b00 + a10 * b01 + a20 * b02;
-//   mat4[1]  = a01 * b00 + a11 * b01 + a21 * b02;
-//   mat4[2]  = a02 * b00 + a12 * b01 + a22 * b02;
-//   mat4[3]  = a03 * b00 + a13 * b01 + a23 * b02;
-//   mat4[4]  = a00 * b10 + a10 * b11 + a20 * b12;
-//   mat4[5]  = a01 * b10 + a11 * b11 + a21 * b12;
-//   mat4[6]  = a02 * b10 + a12 * b11 + a22 * b12;
-//   mat4[7]  = a03 * b10 + a13 * b11 + a23 * b12;
-//   mat4[8]  = a00 * b20 + a10 * b21 + a20 * b22;
-//   mat4[9]  = a01 * b20 + a11 * b21 + a21 * b22;
-//   mat4[10] = a02 * b20 + a12 * b21 + a22 * b22;
-//   mat4[11] = a03 * b20 + a13 * b21 + a23 * b22;
-// }
-
-function createTunnel({depth, depthSubdivs, radialSubdivs}) {
+function createTunnelMesh({depth, depthSubdivs, radialSubdivs}) {
   const positions = new Float32Array(depthSubdivs * radialSubdivs * 3);
   const indices   = new Uint16Array((depthSubdivs-1)*6*(radialSubdivs+1));
 
