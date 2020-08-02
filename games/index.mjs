dc123c22ab79e0ab27937d13f12ece3a23c0cd44
Josh Shone <josh.shone@gmail.com>
2020-08-02 20:40:18 +0200

    Implement #games/bubbleland

--- games/index.mjs
+++ games/index.mjs
@@ -2,12 +2,14 @@ import routes from '/host/routes.mjs';
 
 import '/games/tunnel-vision/host/tunnel-vision.mjs';
 import '/games/show-and-tell/host/show-and-tell.mjs';
+import '/games/bubbleland/host/bubbleland.mjs';
 
 const container = document.createElement('div');
 container.id = 'games-index';
 container.innerHTML = `
   <a href="#games/show-and-tell"><span>Show and Tell</span></a>
   <a href="#games/tunnel-vision">Tunnel Vision</a>
+  <a href="#games/bubbleland">Bubbleland</a>
 `;
 
 document.head.insertAdjacentHTML('beforeend', `
