208c28a643e7e8d4601ea2500e7b81b2428171cc
Etienne Robert <etiennerobert33@gmail.com>
2021-05-13 22:21:48 +0200

    Controls (#2)
    * Add a pp joystick
    
    * Create panels
    
    * Update style

--- apps/remote-control/player/remote-control.mjs
+++ apps/remote-control/player/remote-control.mjs
@@ -1,12 +1,43 @@
 import routes from '/player/routes.mjs';
 
+const getDirection = deadZone => position => {
+  if (position.y > deadZone && position.y > Math.abs(position.x))
+    return "forward";
+  else if (-position.y > deadZone && -position.y > Math.abs(position.x))
+    return "backward";
+  else if (position.x > deadZone && position.x > Math.abs(position.y))
+    return "right";
+  else if (-position.x > deadZone && -position.x > Math.abs(position.y))
+    return "left";
+  else return "static";
+}
+
 routes['#apps/remote-control'] = async function remoteControl({waitForEnd, listenForChannel}) {
   const container = document.createElement('div');
   container.attachShadow({mode: 'open'}).innerHTML = `
     <link rel="stylesheet" href="/apps/remote-control/player/main.css">
-    <h1>Hello remote control buddy!</h1>
+    <div class="panel">
+      <p id="direction">static</p>
+    </div>
+    <div class="panel">
+      <div id="indicators">
+        <div id="forward"></div>
+        <div id="right"></div>
+        <div id="backward"></div>
+        <div id="left"></div>
+      </div>
+      <pp-joystick></pp-joystick>
+    </div>
   `;
+  const shadowRoot = container.shadowRoot;
+
   document.body.append(container);
+
+  const joystick = shadowRoot.querySelector('pp-joystick');
+  joystick.onthumbmove = position => {
+    shadowRoot.getElementById("direction").textContent =
+      getDirection(0.2)(position);
+  }
 
   await waitForEnd();
   container.remove();
