c47cacd376b2e03819bd9835931699cc3f52790b
erobert <etiennerobert33@gmail.com>
2021-05-13 20:01:40 +0200

    Add a pp joystick

--- apps/remote-control/player/remote-control.mjs
+++ apps/remote-control/player/remote-control.mjs
@@ -1,12 +1,33 @@
 import routes from '/player/routes.mjs';
 
+const getDirection = deadZone => position => {
+  if (position.y > deadZone && position.y > Math.abs(position.x))
+    return "forward";
+  else if (-position.y > deadZone && -position.y > Math.abs(position.x))
+    return "backward";
+  else if (position.x > deadZone && position.x > Math.abs(position.y))
+    return "right";
+  else if (-position.x > deadZone && -position.x > Math.abs(position.y))
+    return "left";
+  else return "static";
+}
+
 routes['#apps/remote-control'] = async function remoteControl({waitForEnd, listenForChannel}) {
   const container = document.createElement('div');
   container.attachShadow({mode: 'open'}).innerHTML = `
     <link rel="stylesheet" href="/apps/remote-control/player/main.css">
-    <h1>Hello remote control buddy!</h1>
+    <p id="direction"></p>
+    <pp-joystick></pp-joystick>
   `;
+  const shadowRoot = container.shadowRoot;
+
   document.body.append(container);
+
+  const joystick = shadowRoot.querySelector('pp-joystick');
+  joystick.onthumbmove = position => {
+    shadowRoot.getElementById("direction").textContent =
+      getDirection(0.2)(position);
+  }
 
   await waitForEnd();
   container.remove();
